90bb94e1f3cdb9d040b6171d7c3d6967f8746f7e
journnie <jhyun095@gmail.com>
2025-03-27 11:46:09 +0900

    자산 수정 함수

--- db/db.h
+++ db/db.h
@@ -3,6 +3,7 @@
 #define TOTAL_ASSET_NUM 2
 #define IDX_CASH 0
 #define IDX_STOCK 1
+#include <stdbool.h>
 
 typedef struct User User;
 typedef struct Asset Asset;
@@ -14,7 +15,10 @@ User* db_getUser(const char* user_id);
 Asset* db_getUserAsset(const char* user_id);
 User_Stock* db_getUserStockList(const char *user_id, int *outCount);
 User_Stock* db_getUserStock(const char *user_id, const char *stock_name);
-double db_updateAsset(int asset_id, double delta);
+int db_updateAsset(int asset_id, double delta);
+int db_updateUserStock(const char* user_id, const char* stock_name, int quantity, double price);
+int db_insertUserStock(const char* user_id, const char* stock_name, int quantity, double total_price);
+bool db_checkStockName(char *stock_name);
 
 
 #endif // DB_H
